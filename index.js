bd78a11d302976bc240f21c07c0638665ec5940c
Patrick <pateast678@gmail.com>
2020-01-27 20:10:15 -0500

    makes the project server-friendly

--- index.js
+++ index.js
@@ -1,14 +1,19 @@
+const http = require('http');
+const express = require('express');
+const app = express();
+const server = http.createServer(app);
+const PORT = 3003;
 
-const button = document.getElementsByClassName('but');
-const container = document.querySelector('.container');
+const es6Renderer = require('express-es6-template-engine');                app.engine('html', es6Renderer);
+app.set('views', '');
+app.set('view engine', 'html');
+const path = require('path');
+app.use(express.static(path.join(__dirname, "/")));
 
-function toggler() {
-    console.log(container.style.display)
-    if(container.style.display == "flex"){
-        container.style.display = "none";
-    } else {
-        container.style.display = "flex";
-    }
-}
-toggler();
-// console.log(button.style.display)
+app.get('/', (req, res)=> {
+    res.render('index2')
+})
+
+app.listen(PORT, ()=>{
+    `Now listening on port ${PORT}`
+});
